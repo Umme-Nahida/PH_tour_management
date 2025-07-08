3294a7cce5d6165f6c788ddae5ce4e30aab8ca9f
Developer Nahida <ummenahida20@gmail.com>
2025-07-09 03:37:17 +0600

    firt commit

---
+++ src/server.ts
@@ -0,0 +1,85 @@
+import { Server } from "http"
+import mongoose from "mongoose"
+import app from "./app"
+import { envVars } from "./app/config/env";
+
+const port = 3000
+
+let server: Server;
+let user;
+
+const startServer = async () => {
+
+    try {
+        
+        await mongoose.connect(envVars.DB_Url);
+        console.log('mongoose connected successfully')
+        console.log(envVars.DB_Url)
+        server = app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+startServer();
+
+
+
+process.on('SIGTERM',()=>{
+
+    console.log('sigtern is recieved and shutting down our server..');
+    
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on('SIGINT',()=>{
+
+    console.log('SIGINT SIGNAL is recieved and shutting down our server..');
+    
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on('unhandledRejection',(err)=>{
+
+    console.log('unhandleRejection error is detected',err);
+    
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+// uncaght rejection err handling
+process.on('uncaughtException',(err)=>{
+
+    console.log('uncaughtException error is detected',err);
+    
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+// Promise.reject(new Error("I forgot to handle promise err"))
+// throw new Error("I forgot to handle uncaght error")
+
+// unhandle rejection error 
+// uncaght rejection error
+// signal termination (sigterm)
