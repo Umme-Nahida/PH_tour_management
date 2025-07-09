a9d659a8d1b9f2f5dafe9d413ab5661bceb68601
Developer Nahida <ummenahida20@gmail.com>
2025-07-10 00:34:54 +0600

    added user controlloer

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
-import express from "express"
+import express from "express";
+import cors from "cors"
+import { userRoute } from "./app/modules/user/user.route";
 const app = express()
 
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/v1/user', userRoute)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
