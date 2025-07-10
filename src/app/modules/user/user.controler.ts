939b1ed3661ddaad0ce8aff3033872eec5c539ee
Developer Nahida <ummenahida20@gmail.com>
2025-07-11 03:17:55 +0600

    create user controllers

--- src/app/modules/user/user.controler.ts
+++ src/app/modules/user/user.controler.ts
@@ -1,28 +1,67 @@
-import { Request, Response } from "express";
-import { Users } from "./user.model";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
+import { userServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
-const createUser = async(req:Request,res:Response)=>{
-    try{
+// const createUser = async(req:Request,res:Response, next:NextFunction)=>{
+//     try{
+//     //   throw new AppError(httpStatus.BAD_REQUEST,"fake Error is generated")
+//        const result = await userServices.addUser(req.body)
+        
+//         res.status(httpStatus.CREATED).json({
+//             message: "user created successfully",
+//             result
+//         })
 
-        const {name,email} = req.body;
+//     }catch(err: any){
+//         // res.status(httpStatus.BAD_REQUEST).json({message: ` something went wrong from controller ${err.message}`, err})
+//         next(err)
+//     }
+// }
 
-        const result = await Users.create({
-            name,
-            email
-        })
-        result.save()
-        res.status(httpStatus.CREATED).json({
+
+const createUser = catchAsync(async(req:Request,res:Response, next:NextFunction)=>{
+     const result = await userServices.addUser(req.body)
+        
+        // res.status(httpStatus.CREATED).json({
+        //     message: "user created successfully",
+        //     result
+        // })
+
+        sendResponse(res,{
+            success: true,
+            statusCode: httpStatus.CREATED,
             message: "user created successfully",
-            result
+            data: result
+        })
+})
+
+
+const getAllUser = async(req:Request,res:Response, next:NextFunction)=>{
+    try{
+    //   throw new AppError(httpStatus.BAD_REQUEST,"fake Error is generated")
+       const result = await userServices.getAllUser()
+        
+        sendResponse(res,{
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "All users Retrived successfully",
+            data: result.data,
+            meta: result.meta
         })
 
     }catch(err: any){
-        res.status(httpStatus.BAD_REQUEST).json({"message": ` something went wrong`, err})
+        // res.status(httpStatus.BAD_REQUEST).json({message: ` something went wrong from controller ${err.message}`, err})
+        next(err)
     }
 }
 
 export const userController = {
-    createUser
+    createUser,
+    getAllUser
 }
+
+
+// function => try catch layer => req & res function
