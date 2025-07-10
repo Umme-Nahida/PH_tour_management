e4bbd6e6427c7aa3f886c77f5ec06e606de003d4
Developer Nahida <ummenahida20@gmail.com>
2025-07-11 03:18:21 +0600

    create users route to just handle req and res

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,6 @@ import { userController } from "./user.controler";
 const route = Router();
 
 route.post('/register', userController.createUser);
+route.post('/all-user', userController.getAllUser);
 
 export const userRoute = route;
