57f61f8568d31ca967f531d8722649916b523d61
Developer Nahida <ummenahida20@gmail.com>
2025-07-17 23:52:03 +0600

    solving reset api error

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { Server } from "http"
 import mongoose from "mongoose"
 import app from "./app"
 import { envVars } from "./app/config/env";
+import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
 const port = 5000
 
@@ -24,7 +25,11 @@ const startServer = async () => {
     }
 }
 
-startServer();
+(async()=>{
+   await startServer();
+   await seedSuperAdmin()
+}
+)()
 
 
 
