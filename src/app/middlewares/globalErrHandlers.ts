dcb318bb7dcc5a47f47cf3e93c791baa5c73dd3b
Developer Nahida <ummenahida20@gmail.com>
2025-07-21 02:15:05 +0600

    create division interface

--- src/app/middlewares/globalErrHandlers.ts
+++ src/app/middlewares/globalErrHandlers.ts
@@ -3,52 +3,73 @@ import { envVars } from "../config/env"
 import AppError from "../ErrorHelpers/appError"
 import { object } from "zod"
 
-export const globalErrHandler =(err:any,req:Request,res:Response,next:NextFunction)=>{
+export const globalErrHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    console.log("global:--------",err)
+    if (envVars.node_env === "development") {
+        console.log("global:--------", err)
+    }
 
-       let errorSources:any=[
-        {
-            path:"isDeleted",
-            message:"validation err isDeteled is boolean"
-        }
+    let errorSources: any = [
+        // {
+        //     path:"isDeleted",
+        //     message:"validation err isDeteled is boolean"
+        // }
     ]
 
     let status = 500
     let message = `something went wrong ${err.message}`
-    const errors = Object.values(err.errors)
- 
-    errors.forEach((errObj:any)=>errorSources.push({
-        path:errObj.path,
-        message:errObj.message
-    }))
-    console.log("path:-------",errorSources)
 
-    if(err.name === "CastError"){
-        status=400,
-        message="Please provide valid id"
+
+    if (err.name === "ValidationError") {
+        status=400
+        const errors = Object.values(err.errors)
+
+        errors.forEach((errObj: any) => errorSources.push({
+            path: errObj.path,
+            message: errObj.message
+        }))
+        console.log("path:-------", errorSources)
     }
 
-    if(err.code === 11000){
-        
+    //------------------------------------------ handle zod validation err
+    else if (err.name === "ZodError") {
+        status = 400
+        message = "ZodError"
+
+        err.issues.forEach((issue: any) => errorSources.push({
+            path: issue.path[0],
+            //  path:issue.path[issue.path.length - 1],
+            message: issue.message
+        }))
+    }
+
+    // -----------------------------------------------handle cast Error
+  else if (err.name === "CastError") {
+        status = 400,
+            message = "Please provide valid id"
+    }
+
+  else if (err.code === 11000) {
+
         const dublicate = err.message.match(/"([^"]*)" /)
-        status=400,
-        message=`${dublicate[1]} already exists`
+        status = 400,
+            message = `${dublicate[1]} already exists`
     }
 
-   else if(err instanceof AppError){
+    else if (err instanceof AppError) {
         status = err.statusCode,
-        message= err.message
-    }else if(err instanceof Error){
-        status = 500, 
-        message = err.message
+            message = err.message
+    } else if (err instanceof Error) {
+        status = 500,
+            message = err.message
     }
 
+    console.log(errorSources)
     res.status(status).json({
-       success: false,
-       message: message, 
-       err,
-       errorSources,
-       stack: envVars.node_env ==="development" ? err.stack : null
+        success: false,
+        message: message,
+        err: envVars.node_env === "development" ? err : null,
+        errorSources,
+        stack: envVars.node_env === "development" ? err.stack : null
     })
 }
---
+++ src/app/modules/Division/division.controller.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from "express";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status-codes"
+import { catchAsync } from "../../utils/catchAsync";
+import { divisionServices } from "./division.service";
+
+const createDivisionType = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
+    const division = await divisionServices.createDivision(req.body);
+
+    sendResponse(res,{
+        success:true,
+        statusCode: httpStatus.CREATED,
+        message:"division created successfully",
+        data:division
+    })
+})
+
+
+const getAllDivision = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
+    const division = req.body;
+
+    sendResponse(res,{
+        success:true,
+        statusCode: httpStatus.CREATED,
+        message:"division retrive successfully",
+        data:division
+    })
+})
+
+const updateDivision = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
+    const division = req.body;
+
+    sendResponse(res,{
+        success:true,
+        statusCode: httpStatus.CREATED,
+        message:"division updated successfully",
+        data:division
+    })
+})
+
+
+const deleteDivision = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
+    const division = req.body;
+
+    sendResponse(res,{
+        success:true,
+        statusCode: httpStatus.CREATED,
+        message:" division deleted successfully",
+        data:division
+    })
+})
+
+
+
+export const divisionController = {
+    createDivisionType,
+    getAllDivision,
+    updateDivision,
+    deleteDivision
+}
