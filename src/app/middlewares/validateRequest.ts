650fa08d6c3d7d6376d4401759327d8b919ff53a
Developer Nahida <ummenahida20@gmail.com>
2025-08-04 18:32:14 +0600

    added form data and use urlencoded in middleware for smoothly handle form data

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -5,6 +5,7 @@ import { AnyZodObject } from "zod"
 export const validateCreateUserSchema = (zodSchema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
 
     try {
+        req.body =JSON.parse(req.body.data) || req.body;
         req.body = await zodSchema.parseAsync(req.body)
         console.log("zod req",req.body)
         next()
