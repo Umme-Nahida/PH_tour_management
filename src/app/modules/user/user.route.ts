7806ef01df874c3b1ff3c450c47e3c5d7b42b735
Developer Nahida <ummenahida20@gmail.com>
2025-08-07 14:21:00 +0600

    generate forget password link

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,9 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { userController } from "./user.controler";
 import { createZodSchema } from "./user.createZodSchema";
 import { validateCreateUserSchema } from "../../middlewares/validateRequest";
-import AppError from "../../ErrorHelpers/appError";
-import jwt, { JwtPayload } from "jsonwebtoken"
 import { Role } from "./user.interface";
-import { verifyToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
 import { checkAuth } from "../../utils/checkAuth";
-import { object } from "zod";
 
 const route = Router();
 
@@ -19,6 +14,7 @@ route.post('/register',
     userController.createUser);
 
 route.get('/all-user',checkAuth(Role.ADMIN,Role.SUPER_ADMIN), userController.getAllUser);
+route.get('/getMe',checkAuth(Role.ADMIN,Role.SUPER_ADMIN), userController.getMe);
 route.patch('/:id',checkAuth(...Object.keys(Role)), userController.updateUser);
 
 export const userRoute = route;
