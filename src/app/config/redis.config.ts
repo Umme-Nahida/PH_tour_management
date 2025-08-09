9a23d30317f7514f0bb5bd4b4c8b0df8568140b7
Developer Nahida <ummenahida20@gmail.com>
2025-08-10 00:01:27 +0600

    created ootp service

---
+++ src/app/config/redis.config.ts
@@ -0,0 +1,25 @@
+import { createClient } from 'redis';
+import { envVars } from './env';
+
+const client = createClient({
+    username: envVars.REDIS_USERNAME,
+    password: envVars.REDIS_PASSWORD,
+    socket: {
+        host: envVars.REDIS_HOST,
+        port: parseInt(envVars.REDIS_PORT)
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+
+// await client.set('foo', 'bar');
+// const result = await client.get('foo');
+// console.log(result)  // >>> bar
+
+export const connectRedis = async () => {
+    if (!client.isOpen) {
+        await client.connect();
+        console.log("redis connected")
+    }
+}
